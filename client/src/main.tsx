82ca2b6918ae0c150b08d2c66cd2eb013dc55990
Saif ur Rehman <saifr@seed.im>
2023-03-25 23:56:06 +0500

    redux toolkit configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import '@/index.css'
 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from "@/state/api"
+
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+})
+
+setupListeners(store.dispatch)
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
---
+++ client/src/state/api.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+    reducerPath: "main",
+    tagTypes: ["Kpis"],
+    endpoints: (build) => ({
+        getKpis: build.query<void, void>({
+            query: () => "kpi/kpis",
+
+            // tag to identify
+            providesTags: ["Kpis"],
+        }),
+    }),
+});
+
+export const { useGetKpisQuery } = api;

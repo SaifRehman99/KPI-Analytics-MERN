0ee512a21af3f4ed47df11c1340d19636413be13
Saif ur Rehman <saifr@seed.im>
2023-03-26 00:48:59 +0500

    mongodb setup and connection

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import mongoose from "morgan";
+import mongoose from "mongoose";
 
 /* CONFIGURATION */
-dotenv.config();
+dotenv.config({});
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -15,3 +15,17 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 
 /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+
+mongoose
+    .connect(`${process.env.MONGO_URI}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(async () => {
+        app.listen(PORT, () => console.log("listening on port " + PORT));
+    })
+    .catch((error) => {
+        console.log(`${error} didn't connect`);
+        process.exit(1);
+    });

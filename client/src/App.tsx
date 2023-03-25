71e705cd60e91d88ad1c56e5124f8a4c7ea1566f
Saif ur Rehman <saifr@seed.im>
2023-03-25 22:28:37 +0500

    react router and navbar config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,32 @@
 import { useMemo } from "react"
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { CssBaseline, ThemeProvider, Box } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 import { themeSettings } from "@/theme"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Navbar from "@/scenes/navbar"
 
 function App() {
 
   const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
-
-
     <div className="App">
-      <ThemeProvider theme={theme}>
-        {/* reseting the css into def material styles */}
-        <CssBaseline />
-        hi
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          {/* reseting the css into def material styles */}
+          <CssBaseline />
+
+          <Box width="100%" height="100" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+            </Routes>
+
+          </Box>
+
+        </ThemeProvider>
+      </BrowserRouter>
 
     </div>
   )
